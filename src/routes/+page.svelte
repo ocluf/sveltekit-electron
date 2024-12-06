7fe97a8e8a380945b9b179cce90afb4d12e0c3ce
ocluf <fulco.taen@gmail.com>
2024-12-06 22:29:02 +0100

    make the inital screen a tiny bit prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
-<h1 class="bg-gray-200">Welcome to SvelteKit</h1>
-<a href="/settings">Settings</a>
-<div class="flex gap-4 mt-4">
-	<button
-		class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-		onclick={() => window.api.preload()}
-	>
-		Call Preload Function
-	</button>
+<div class="flex flex-col items-center justify-center min-h-screen">
+	<h1 class="text-2xl font-bold">Welcome to SvelteKit + Electron</h1>
+	<a href="/settings" class="text-blue-500 underline">Go to settings</a>
+	<div class="flex flex-row gap-4 mt-4">
+		<div class="flex flex-col items-center w-80 p-4 border rounded-lg shadow">
+			<button
+				class="w-64 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 active:scale-95 transition-transform cursor-pointer"
+				onclick={() => window.api.preload()}
+			>
+				Preload Context Log
+			</button>
+			<span class="text-sm text-center opacity-75 mt-2">
+				Check electron/preload/index.ts and DevTools Console <strong>(F12)</strong>
+			</span>
+		</div>
 
-	<button
-		class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-		onclick={() => window.api.main()}
-	>
-		Call Main Function
-	</button>
+		<div class="flex flex-col items-center w-80 p-4 border rounded-lg shadow">
+			<button
+				class="w-64 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 active:scale-95 transition-transform cursor-pointer"
+				onclick={() => window.api.main()}
+			>
+				Main Process Log
+			</button>
+			<span class="text-sm text-center opacity-75 mt-2">
+				Check electron/main/index.ts and Main Process logs
+			</span>
+		</div>
+	</div>
 </div>
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,1 +1,4 @@
-<a href="/">Home</a>
+<div class="flex flex-col items-center justify-center min-h-screen">
+	<h1 class="text-2xl font-bold">There are no settings but routing works as expected</h1>
+	<a href="/" class="text-blue-500 underline"> Go back Home</a>
+</div>
