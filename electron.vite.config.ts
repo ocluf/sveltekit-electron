aae0a43e21f6b3e96a143611968f3ab2857a9f97
ocluf <fulco.taen@gmail.com>
2024-12-06 23:41:51 +0100

    add renderer back from hot reloading in dev, add shadcn

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+// @ts-ignore
+import { sveltekit } from '@sveltejs/kit/vite';
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 const config = defineConfig({
 	main: {
@@ -19,6 +24,17 @@ const config = defineConfig({
 				formats: ['cjs'],
 				entry: './electron/preload/index.ts'
 			}
+		}
+	},
+	renderer: {
+		plugins: [sveltekit(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@renderer': resolve('src')
+			}
+		},
+		build: {
+			target: ['chrome126']
 		}
 	}
 });
